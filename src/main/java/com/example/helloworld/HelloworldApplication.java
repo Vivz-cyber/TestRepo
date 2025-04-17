39aa0584ef84eb2c91967e4769b01cf1e9b54046
Vivek Vatsa <72134769+Vivz-cyber@users.noreply.github.com>
2025-04-17 20:04:06 +0530

    Update HelloworldApplication.java

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -3,9 +3,17 @@ package com.example.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloworldApplication extends SpringBootServletInitializer {
+    
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
     public static void main(String[] args) {
         SpringApplication.run(HelloworldApplication.class, args);
     }
